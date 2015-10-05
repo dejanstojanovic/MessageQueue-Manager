bae53efa61ecd40607d4e9545ccb64025da6f4cb
Dejan Stojanovic <d.stojanovic@hotmail.com>
2015-10-05 14:58:20 +0400

    Added sample application

---
+++ src/MessageQueuing.TestApplication/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageQueuing.TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queueManager = new MessageQueueManager<SampleModel>(@".\private$\TestQueue");
+
+            for(int i = 0; i < 13; i++)
+            {
+                queueManager.AddMessage(new SampleModel() { ID = Guid.NewGuid().ToString(), TimeCreated=DateTime.Now });
+            }
+
+            queueManager.MessageReceived += QueueManager_MessageReceived;
+
+            Console.ReadLine();
+        }
+
+        private static void QueueManager_MessageReceived(object sender, MessageReceivedEventArgs<SampleModel> e)
+        {
+            Console.WriteLine("Message received {0} {1}", e.Message.ID, e.Message.TimeCreated.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+        }
+    }
+}
---
+++ src/MessageQueuing.TestApplication/SampleModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageQueuing.TestApplication
+{
+    public class SampleModel
+    {
+        public string ID
+        {
+            get; set;
+        }
+        public DateTime TimeCreated
+        {
+            get; set;
+        }
+    }
+}
