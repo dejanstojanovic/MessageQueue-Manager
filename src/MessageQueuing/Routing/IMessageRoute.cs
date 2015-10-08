e9c23f088fb7a6f43e09bd46fb837389d9940939
Dejan Stojanovic <d.stojanovic@hotmail.com>
2015-10-08 17:04:26 +0400

    Added message router (initial commit)

---
+++ src/MessageQueuing/Routing/IMessageRoute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageQueuing.Routing
+{
+    public interface IMessageRoute<T>
+    {
+        int Order { get; }
+        string DestinationQueue { get; }
+
+        bool ProcessMessage(T message);
+    }
+}
---
+++ src/MessageQueuing/Routing/IMessageRouter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageQueuing.Routing
+{
+    public interface IMessageRouter<T>
+    {
+        string FallbackQueue { get; }
+        IEnumerable<IMessageRoute<T>> Routes { get; }
+        void Route(T message);
+
+
+     
+    }
+}
