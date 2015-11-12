92062ca41d8056b8de3149d0fc1f05c9407664a4
Dejan Stojanovic <d.stojanovic@hotmail.com>
2015-11-13 02:10:45 +0400

    Added ordered tests

---
+++ src/MessageQueuing.Tests/QueueManager/ReadTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MessageQueuing.Tests.QueueManager
+{
+    [TestClass]
+    public class ReadTests
+    {
+        [TestMethod]
+        public void ReadXml()
+        {
+        }
+
+        [TestMethod]
+        public void ReadJson()
+        {
+        }
+    }
+}
---
+++ src/MessageQueuing.Tests/QueueManager/WriteTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MessageQueuing.Tests.QueueManager
+{
+    [TestClass]
+    public class WriteTests
+    {
+        [TestMethod]
+        public void TestWriteXml()
+        {
+            
+        }
+
+        [TestMethod]
+        public void TestWriteJson()
+        {
+        }
+
+
+    }
+}
